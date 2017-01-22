352d29c2caa074b8ea55e90839bc4544a2d82d6b
hidefromkgb <hidefromkgb@gmail.com>
2017-01-22 09:30:52 +0300

    Add keymap to the MacOS branch

--- macos/main.c
+++ macos/main.c
@@ -45,72 +45,46 @@ void MAC_Handler(OnDraw, CGRect rect) {
     flushBuffer(openGLContext(self));
 }
 
-void MAC_Handler(OnKbd, NSEvent *ekbd) {
-    CFStringRef temp;
+void MAC_Handler(OnKeys, NSEvent *ekey) {
+    static uint8_t keys[256] = { /** see the list of kVK_* for info **/
+        KEY_A         , KEY_S         , KEY_D         , KEY_F         ,
+        KEY_H         , KEY_G         , KEY_Z         , KEY_X         ,
+        KEY_C         , KEY_V         , KEY_NONE      , KEY_B         ,
+        KEY_Q         , KEY_W         , KEY_E         , KEY_R         ,
+        KEY_Y         , KEY_T         , KEY_1         , KEY_2         ,
+        KEY_3         , KEY_4         , KEY_6         , KEY_5         ,
+        KEY_EQUALS    , KEY_9         , KEY_7         , KEY_HYPHEN    ,
+        KEY_8         , KEY_0         , KEY_RBRACKET  , KEY_O         ,
+        KEY_U         , KEY_LBRACKET  , KEY_I         , KEY_P         ,
+        KEY_ENTER     , KEY_L         , KEY_J         , KEY_QUOTE     ,
+        KEY_K         , KEY_COLON     , KEY_PIPE      , KEY_LESS      ,
+        KEY_QUESTION  , KEY_N         , KEY_M         , KEY_GREATER   ,
+        KEY_TAB       , KEY_SPACE     , KEY_TILDE     , KEY_BACKSPACE ,
+        KEY_NONE      , KEY_ESC       , KEY_NONE      , KEY_LALT      ,
+        KEY_LSHIFT    , KEY_CAPSLOCK  , KEY_LSYSTEM   , KEY_LCTRL     ,
+        KEY_RSHIFT    , KEY_RSYSTEM   , KEY_RCTRL     , KEY_NONE      ,
+        KEY_NONE      , KEY_NUM_DEL   , KEY_NONE      , KEY_NUM_MUL   ,
+        KEY_NONE      , KEY_NUM_ADD   , KEY_NONE      , KEY_NONE      ,
+        KEY_NONE      , KEY_NONE      , KEY_NONE      , KEY_NUM_DIV   ,
+        KEY_NUM_ENTER , KEY_NONE      , KEY_NUM_SUB   , KEY_NONE      ,
+        KEY_NONE      , KEY_EQUALS    , KEY_NUM_0     , KEY_NUM_1     ,
+        KEY_NUM_2     , KEY_NUM_3     , KEY_NUM_4     , KEY_NUM_5     ,
+        KEY_NUM_6     , KEY_NUM_7     , KEY_NONE      , KEY_NUM_8     ,
+        KEY_NUM_9     , KEY_NONE      , KEY_NONE      , KEY_NONE      ,
+        KEY_F5        , KEY_F6        , KEY_F7        , KEY_F3        ,
+        KEY_F8        , KEY_F9        , KEY_NONE      , KEY_F11       ,
+        KEY_NONE      , KEY_NONE      , KEY_NONE      , KEY_NONE      ,
+        KEY_NONE      , KEY_F10       , KEY_NONE      , KEY_F12       ,
+        KEY_NONE      , KEY_NONE      , KEY_NONE      , KEY_HOME      ,
+        KEY_PAGEUP    , KEY_DELETE    , KEY_F4        , KEY_END       ,
+        KEY_F2        , KEY_PAGEDOWN  , KEY_F1        , KEY_LEFT      ,
+        KEY_RIGHT     , KEY_DOWN      , KEY_UP        ,
+        /** only KEY_NONE`s here... **/
+    };
     ENGC *engc;
-    long down;
 
-    if (!CFStringGetLength(temp = charactersIgnoringModifiers(ekbd)))
-        return;
     MAC_GetIvar(self, VAR_ENGC, &engc);
-    down = (type(ekbd) == NSKeyDown)? 1 : 0;
-    switch (CFStringGetCharacterAtIndex(temp, 0)) {
-        case NSLeftArrowFunctionKey:
-            cKbdInput(engc, KEY_LEFT, down);
-            break;
-
-        case NSRightArrowFunctionKey:
-            cKbdInput(engc, KEY_RIGHT, down);
-            break;
-
-        case NSUpArrowFunctionKey:
-            cKbdInput(engc, KEY_UP, down);
-            break;
-
-        case NSDownArrowFunctionKey:
-            cKbdInput(engc, KEY_DOWN, down);
-            break;
-
-        case NSF1FunctionKey:
-            cKbdInput(engc, KEY_F1, down);
-            break;
-
-        case NSF2FunctionKey:
-            cKbdInput(engc, KEY_F2, down);
-            break;
-
-        case NSF3FunctionKey:
-            cKbdInput(engc, KEY_F3, down);
-            break;
-
-        case NSF4FunctionKey:
-            cKbdInput(engc, KEY_F4, down);
-            break;
-
-        case NSF5FunctionKey:
-            cKbdInput(engc, KEY_F5, down);
-            break;
-
-        case ' ':
-            cKbdInput(engc, KEY_SPACE, down);
-            break;
-
-        case 'w': case 'W':
-            cKbdInput(engc, KEY_W, down);
-            break;
-
-        case 's': case 'S':
-            cKbdInput(engc, KEY_S, down);
-            break;
-
-        case 'a': case 'A':
-            cKbdInput(engc, KEY_A, down);
-            break;
-
-        case 'd': case 'D':
-            cKbdInput(engc, KEY_D, down);
-            break;
-    }
+    cKbdInput(engc, keys[keyCode(ekey) & 0xFF], !!(type(ekey) == NSKeyDown));
 }
 
 void OnInvalidate(CFRunLoopTimerRef tmrp, void *user) {
@@ -173,8 +147,8 @@ int main(int argc, char *argv[]) {
     vogl = MAC_MakeClass("NSO", NSOpenGLView(), MAC_TempArray(VAR_ENGC),
                          MAC_TempArray(drawRect_(), OnDraw,
                                        windowDidResize_(), OnSize,
-                                       keyDown_(), OnKbd, keyUp_(), OnKbd,
                                        windowShouldClose_(), OnClose,
+                                       keyDown_(), OnKeys, keyUp_(), OnKeys,
                                        acceptsFirstResponder(), OnTrue));
 
     pfmt = initWithAttributes_(alloc(NSOpenGLPixelFormat()), attr);
